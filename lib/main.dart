f2c6a03df26e0a9640347f742a21f6f04c062e51
rojicp <rojicp@gmail.com>
2023-07-13 20:04:41 +0530

    13th July - Counter

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,91 @@ void main() {
   runApp(const TrainingApp());
 }
 
-class TrainingApp extends StatelessWidget {
+class TrainingApp extends StatefulWidget {
   const TrainingApp({super.key});
+
+  @override
+  State<TrainingApp> createState() => _TrainingAppState();
+}
+
+class _TrainingAppState extends State<TrainingApp> {
+  double count = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "This is a Title",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My First Page"),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  count++;
+                });
+
+                print(count);
+              },
+              child: const Row(
+                children: [Icon(Icons.add), Text("Add")],
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  count = 0;
+                });
+
+                print(count);
+              },
+              child: const Icon(Icons.restore),
+            ),
+          ],
         ),
-        body: const Center(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              const Text("My First Page"),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    count++;
+                    setState(() {});
+                  },
+                  child: const Text("Plus")),
+              const SizedBox(
+                width: 5,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    count++;
+                  });
+
+                  print(count);
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+        body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              "First Line Text",
+            const Text(
+              "Below given the current count",
               style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: Colors.green),
             ),
             Text(
-              "Second Line",
-              style: TextStyle(
-                  fontSize: 15, fontWeight: FontWeight.bold, color: Colors.red),
+              count.toString(),
+              style: const TextStyle(
+                  fontSize: 55, fontWeight: FontWeight.bold, color: Colors.red),
             ),
           ],
         )),
